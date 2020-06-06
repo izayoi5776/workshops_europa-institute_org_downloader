4857fc4321d3a19acccf5d546dbd81f2cbd9222e
izayoi5776 <izayoi5776@gmail.com>
2020-06-06 18:35:48 +0900

    download small resolution and resize image 1st to avoid hi-res version fail

--- downloadImage.py
+++ downloadImage.py
@@ -6,18 +6,47 @@ import functools
 import json
 import re
 from PIL import Image
+import math
 
 # download image parts and merge to a full version
 def downloadImg2(base, url, filename, width, height):
-  # url = "https://iiif.directories.europa-institute.org/image/294849_0000/full/full/0/native.jpg"
-  # target https://iiif.directories.europa-institute.org/image/294849_0000/0,2048,1024,1024/1024,/0/default.jpg
-  #                                                                        x y    w    h    w
+  # url             = "https://iiif.directories.europa-institute.org/image/294849_0000/full/full/0/native.jpg"
+  # small-full-version https://iiif.directories.europa-institute.org/image/294849_0000/full/530,/0/default.jpg
+  #                                                                                         ceil(width/4)
+  # parts-version      https://iiif.directories.europa-institute.org/image/294849_0000/0,2048,1024,1024/1024,/0/default.jpg
+  # parts-version      https://iiif.directories.europa-institute.org/image/294849_0000/0,2048,1024,1024/1024,/0/default.jpg
+  #                                                                                    x y    w    h    w
   url2 = url.replace("full/full/0/native.jpg", "")
   #print("downloadImg2 base=" + base + " url=" + url + " filename=" + filename + " width=" + str(width) + " height=" + str(height))
   
   failmsg = ""
-
   img = Image.new('RGB', (width, height))
+
+  # a small-full-version in one img
+  u = url2 + "full/" + str(math.ceil(width / 4)) + ",/0/default.jpg"
+  try:
+    c = Image.open(urllib.request.urlopen(u))
+    print("downloadImg2 " + u + " OK")
+    img.paste(c.resize((width, height)), (0, 0))
+  except Exception as ex:
+    print("downloadImg2 " + u + " FAILED")
+    failmsg = " with fails"
+
+  # a half version
+  for x in range(0, width, 2048):
+    for y in range(0, height, 2048):
+      w = 2048 if x+2048<width else width-x
+      h = 2048 if y+2048<height else height-y
+      u = url2 + str(x) + "," + str(y) + "," + str(w) + "," + str(h) + "/" + str(int(w/2)) + ",/0/default.jpg"
+      try:
+        c = Image.open(urllib.request.urlopen(u))
+        print("downloadImg2 " + u + " OK")
+        img.paste(c.resize((w, h)), (x, y))
+      except Exception as ex:
+        print("downloadImg2 " + u + " FAILED")
+        failmsg = " with fails"
+
+  # a full version
   for x in range(0, width, 1024):
     for y in range(0, height, 1024):
       w = 1024 if x+1024<width else width-x
